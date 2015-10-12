34ad303a30504fe3cbf7ef828572f73d2cbbe9e7
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-10-12 18:32:31 +0300

    A few "contratuple" functions

--- library/Contravariant/Extras.hs
+++ library/Contravariant/Extras.hs
@@ -4,14 +4,20 @@ import BasePrelude
 import Data.Functor.Contravariant
 import Data.Functor.Contravariant.Divisible
 import qualified Contravariant.Extras.TH as TH
+import qualified TupleTH
 
 
-{-# INLINE premap #-}
-premap :: Contravariant f => (a -> b) -> f b -> f a
-premap =
-  contramap
+contratuple2 :: Divisible f => f a1 -> f a2 -> f ( a1 , a2 )
+contratuple2 f1 f2 =
+  divide id f1 f2
 
-{-# INLINE premap2 #-}
-premap2 :: Divisible f => (a -> (b, c)) -> f b -> f c -> f a
-premap2 =
-  divide
+contratuple3 :: Divisible f => f a1 -> f a2 -> f a3 -> f ( a1 , a2 , a3 )
+contratuple3 f1 f2 f3 =
+  divide $(TupleTH.splitTupleAt 3 1) f1 $
+  contratuple2 f2 f3
+
+contratuple4 :: Divisible f => f a1 -> f a2 -> f a3 -> f a4 -> f ( a1 , a2 , a3 , a4 )
+contratuple4 f1 f2 f3 f4 =
+  divide $(TupleTH.splitTupleAt 4 1) f1 $
+  contratuple3 f2 f3 f4
+
