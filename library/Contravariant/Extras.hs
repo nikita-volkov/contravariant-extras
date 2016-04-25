49b470c73b432933fff34dc61073e4deee0d21b4
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-04-25 17:12:05 +0300

    "contramany"

--- library/Contravariant/Extras.hs
+++ library/Contravariant/Extras.hs
@@ -3,7 +3,30 @@ module Contravariant.Extras
   -- |
   -- A berserk collection of @contrazip@ functions with arities of up to 42.
   module Contravariant.Extras.Contrazip,
+  contramany,
 )
 where
 
+import BasePrelude
 import Contravariant.Extras.Contrazip
+import Data.Functor.Contravariant.Divisible
+
+
+contramany :: Decidable f => f a -> f [a]
+contramany f =
+  loop
+  where
+    loop =
+      choose chooser cons nil
+      where
+        chooser =
+          \case
+            head : tail ->
+              Left (head, tail)
+            _ ->
+              Right ()
+        cons =
+          divide id f loop
+        nil =
+          conquer
+
