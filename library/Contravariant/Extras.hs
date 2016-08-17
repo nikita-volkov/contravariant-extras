c24f39cc90435a7aa4d618f98b9a76ae32bdada4
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-08-17 18:56:55 +0300

    Add Supplied

--- library/Contravariant/Extras.hs
+++ library/Contravariant/Extras.hs
@@ -4,6 +4,7 @@ module Contravariant.Extras
   -- A berserk collection of @contrazip@ functions with arities of up to 42.
   module Contravariant.Extras.Contrazip,
   contramany,
+  Supplied,
 )
 where
 
@@ -29,4 +30,21 @@ contramany f =
           divide id f loop
         nil =
           conquer
+
+-- |
+-- A combination of a divisible functor with some input for it.
+-- Allows to use the 'Monoid' API for composition.
+data Supplied divisible =
+  forall input. Supplied !(divisible input) !input
+
+instance Divisible divisible => Monoid (Supplied divisible) where
+  mempty =
+    Supplied conquer ()
+  mappend (Supplied divisible1 input1) (Supplied divisible2 input2) =
+    Supplied divisible3 input3
+    where
+      divisible3 =
+        divide id divisible1 divisible2
+      input3 =
+        (input1, input2)
 
