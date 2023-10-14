363d6b2a16c286956fa5647afe50d61a3d4b9940
nikita-volkov <nikita-volkov@users.noreply.github.com>
2023-10-14 23:19:19 +0000

    Format

--- library/Contravariant/Extras.hs
+++ library/Contravariant/Extras.hs
@@ -1,34 +1,30 @@
 module Contravariant.Extras
-(
-  {-|
-  @contrazip@ functions of multiple arities.
-  -}
-  module Contravariant.Extras.Contrazip,
-  {-|
-  @contrazipLifting@ functions of multiple arities.
-  -}
-  module Contravariant.Extras.ContrazipLifting,
-  (>*<),
-  contramany,
-  Supplied(..),
-)
+  ( -- |
+    --  @contrazip@ functions of multiple arities.
+    module Contravariant.Extras.Contrazip,
+    -- |
+    --  @contrazipLifting@ functions of multiple arities.
+    module Contravariant.Extras.ContrazipLifting,
+    (>*<),
+    contramany,
+    Supplied (..),
+  )
 where
 
-import Contravariant.Extras.Prelude hiding ((<>))
 import Contravariant.Extras.Contrazip
 import Contravariant.Extras.ContrazipLifting
+import Contravariant.Extras.Prelude hiding ((<>))
 import Data.Functor.Contravariant.Divisible
 import Data.Semigroup (Semigroup ((<>)))
-
 
 -- |
 -- An alias to 'divided'.
 {-# INLINE (>*<) #-}
-(>*<) :: Divisible f => f a -> f b -> f (a, b)
+(>*<) :: (Divisible f) => f a -> f b -> f (a, b)
 (>*<) =
   divided
 
-contramany :: Decidable f => f a -> f [a]
+contramany :: (Decidable f) => f a -> f [a]
 contramany f =
   loop
   where
@@ -49,10 +45,10 @@ contramany f =
 -- |
 -- A combination of a divisible functor with some input for it.
 -- Allows to use the 'Monoid' API for composition.
-data Supplied divisible =
-  forall input. Supplied (divisible input) input
+data Supplied divisible
+  = forall input. Supplied (divisible input) input
 
-instance Divisible divisible => Semigroup (Supplied divisible) where
+instance (Divisible divisible) => Semigroup (Supplied divisible) where
   Supplied divisible1 input1 <> Supplied divisible2 input2 =
     Supplied divisible3 input3
     where
@@ -61,7 +57,7 @@ instance Divisible divisible => Semigroup (Supplied divisible) where
       input3 =
         (input1, input2)
 
-instance Divisible divisible => Monoid (Supplied divisible) where
+instance (Divisible divisible) => Monoid (Supplied divisible) where
   mempty =
     Supplied conquer ()
   mappend =
--- library/Contravariant/Extras/Op.hs
+++ library/Contravariant/Extras/Op.hs
@@ -1,11 +1,10 @@
 -- |
 -- This module exports functions specialized for the `Op` type.
 module Contravariant.Extras.Op
-(
-  -- |
-  -- A berserk collection of @contrazip@ functions with arities of up to 42.
-  module Contravariant.Extras.Op.Contrazip,
-)
+  ( -- |
+    -- A berserk collection of @contrazip@ functions with arities of up to 42.
+    module Contravariant.Extras.Op.Contrazip,
+  )
 where
 
 import Contravariant.Extras.Op.Contrazip
